966ce4174b8ab634ef95472157e29ddc3a4b5a9c
justintd <justintdinh1@gmail.com>
2020-10-13 03:07:33 -0700

    Added postcss, config files, and initalized NavBar component

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <div className="flex justify-between items-center">
+      <p className="font-sans text-lg">test test test</p>
+    </div>
+  );
+}
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import Head from 'next/head';
+import NavBar from '../components/NavBar/NavBar';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+    </div>
+  );
 }
